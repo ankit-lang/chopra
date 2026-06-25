060bda84179c95880893115a98b7ff6d08763bc5
Ankit kumar <73269179+ankit-lang@users.noreply.github.com>
2026-06-25 17:20:17 +0530

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -91,15 +91,19 @@ export default function LocaleLayout({
   return (
     <html lang={params.locale} className={`${cormorant.variable} ${dmSans.variable} ${greatVibes.variable}`}>
       <head>
-        <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZKMF6VZ9Q2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-ZKMF6VZ9Q2');
-</script>
+      <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-ZKMF6VZ9Q2"
+    strategy="afterInteractive"
+  />
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-ZKMF6VZ9Q2');
+    `}
+  </Script>
+        
         <link rel="profile" href="https://chopras.nl/llms.txt" />
         <Script
           id="gtm-script"
